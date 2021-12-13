5204d5d007ab33eb182fcd78e00ff94f33e5f972
Philippe Chepy <philippe@chepy.eu>
2021-12-13 23:07:30 +0100

    Add README and variable/outputs descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "certificate_bundle_pem" {
-  value = "${tls_private_key.intermediate.private_key_pem}${tls_locally_signed_cert.intermediate.cert_pem}"
+  description = "A bundle containing the private key and the resulting certificate."
+  value       = "${tls_private_key.intermediate.private_key_pem}${tls_locally_signed_cert.intermediate.cert_pem}"
 }
 
 output "private_key_pem" {
-  value = tls_private_key.intermediate.private_key_pem
+  description = "The private key of the certificate."
+  value       = tls_private_key.intermediate.private_key_pem
 }
 
 output "certificate_pem" {
-  value = tls_locally_signed_cert.intermediate.cert_pem
+  description = "The resulting certificate."
+  value       = tls_locally_signed_cert.intermediate.cert_pem
 }
