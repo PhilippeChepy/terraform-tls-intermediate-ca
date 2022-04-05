28a42ce01a4ceaad0aeea59020ad4579887bb832
Philippe Chepy <philippe@chepy.eu>
2022-04-05 22:54:22 +0200

    Add subject variable to handle all subject fields

--- main.tf
+++ main.tf
@@ -9,7 +9,14 @@ resource "tls_cert_request" "intermediate" {
   private_key_pem = tls_private_key.intermediate.private_key_pem
 
   subject {
-    common_name = var.common_name
+    common_name         = var.subject.common_name != "" ? var.subject.common_name : var.common_name
+    organizational_unit = try(var.subject.organizational_unit)
+    organization        = try(var.subject.organization)
+    street_address      = try(var.subject.street_address)
+    postal_code         = try(var.subject.postal_code)
+    locality            = try(var.subject.locality)
+    province            = try(var.subject.province)
+    country             = try(var.subject.country)
   }
 }
 
